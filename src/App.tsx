0fb5cab01cd1cf3c74265a9489bff017f03ebb01
Akash Shyam <akash.shyam2008@gmail.com>
2021-11-23 22:49:44 +0530

    chore: Initialised toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { router } from "./domain/router";
 import SignIn from "./domain/signin/SignIn";
+import theme from "./theme/theme";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -10,6 +13,8 @@ export const App = () => (
       <Routes>
         <Route path={router.signin.path} element={<SignIn />}></Route>
       </Routes>
+
+      <ToastContainer />
     </BrowserRouter>
   </ChakraProvider>
 );
